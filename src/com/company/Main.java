5f48d68b2ec2eb113b6cc546ca8efc6728c6f434
azemzyulin <azemzyulin@gmail.com>
2021-05-23 20:34:44 +0700

    Initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Java!");
+        System.out.println("Hello Java2");
+        System.out.println("Hello Java3");
 	// write your code here
     }
 }
