b94c415c7e979681c7394c6cb8971e5663adee16
azemzyulin <azemzyulin@gmail.com>
2021-05-25 14:42:08 +0700

    Initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,17 +4,20 @@ import java.lang.management.MemoryUsage;
 
 //import java.util.Scanner;
 public class Main {
-    public static int[][] MULTIPLICATION_TABLE;
+    public static int[][] result = new int[10][];
+
     public static void main(String[] args) {
         {
-            MULTIPLICATION_TABLE = new int[10][10];
             for (int i = 0; i < 10; i++) {
-                for (int j = 0; j < 10; j++) {
-                    MULTIPLICATION_TABLE[i][j] = (i + 1) * (j + 1);
-                    if (j < 9) {
-                        System.out.print(MULTIPLICATION_TABLE[i][j] + " ");
+                result[i] = new int[i + 1];
+            }
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < result[i].length; j++) {
+                    result[i][j] = i + j;
+                    if (j < result[i].length - 1) {
+                        System.out.print(result[i][j] + " ");
                     } else {
-                        System.out.println(MULTIPLICATION_TABLE[i][j]);
+                        System.out.println(result[i][j]);
                     }
                 }
             }
