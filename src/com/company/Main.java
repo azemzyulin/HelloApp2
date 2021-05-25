d4f0aa04cad1a69e73cc1844ce5e9267b3fbc346
azemzyulin <azemzyulin@gmail.com>
2021-05-25 14:30:06 +0700

    Initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,23 @@
 package com.company;
 
-public class Main {
+import java.lang.management.MemoryUsage;
 
+//import java.util.Scanner;
+public class Main {
+    public static int[][] MULTIPLICATION_TABLE;
     public static void main(String[] args) {
-        System.out.println("Hello Java!");
-        System.out.println("Hello Java2");
-        System.out.println("Hello Java3");
-        System.out.println("Hello Java4");
-	// write your code here
+        {
+            MULTIPLICATION_TABLE = new int[10][10];
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    MULTIPLICATION_TABLE[i][j] = (i + 1) * (j + 1);
+                    if (j < 9) {
+                        System.out.print(MULTIPLICATION_TABLE[i][j] + " ");
+                    } else {
+                        System.out.println(MULTIPLICATION_TABLE[i][j]);
+                    }
+                }
+            }
+        }
     }
 }
